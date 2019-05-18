37f15503d2047d699c05317933ff5f61d7bca649
mohidqureshi <mohidq99@gmail.com>
2019-05-18 10:46:48 -0400

    basic mobile to web conversion

--- popup.js
+++ popup.js
@@ -1,43 +1,61 @@
 let link = document.getElementById('convertButton')
 let clear = document.getElementById('clearButton')
 chrome.storage.sync.get(['userText'], function(result) {
-	document.getElementById('convertedUrl').textContent = result.userText
-
-});
+                        
+                        document.getElementById('convertedUrl').textContent = result.userText
+                        
+                        });
 
 //function to convert mobile link to web link
 link.onclick = function() {
-	let defaultLink = "https://item.taobao.com/item.htm?id="
-	//document.getElementById('link').value = 'works'
-	document.getElementById('convertedUrl').textContent = defaultLink;
-
-	chrome.storage.sync.set({userText: document.getElementById('convertedUrl').textContent}, function() {
-          
-    });
-
-	//use textContent to change text
+    let defaultLink = "https://item.taobao.com/item.htm?id="
+    
+    let preURL = document.getElementById('link').value
+    let id = "";
+    
+    //if link entered does not contain an id query
+    if(!preURL.includes("id=")) {
+        document.getElementById('errorMessage').textContent = "Enter Valid Link"
+    }
+    else {
+        document.getElementById('errorMessage').textContent = ""
+        
+        let index = preURL.indexOf("id=") + 3
+        id = preURL.substring(index, index + 12)
+        document.getElementById('convertedUrl').textContent = defaultLink + id;
+    }
+    
+    document.getElementById('link').value = ''
+    //document.getElementById('convertedUrl').textContent = defaultLink;
+    //document.getElementById('linkTab').innerHTML += `<h2 name = "cl">${10}</h2>`;
+    chrome.storage.sync.set({userText: document.getElementById('convertedUrl').textContent}, function() {
+                            
+                            });
+    
+    //use textContent to change text
 }
 
+//when clear button id clicked
 clear.onclick = function() {
-	chrome.storage.sync.remove(['userText'], function() {
-		document.getElementById("convertedUrl").textContent = ""
-		document.getElementById("link").value = ""
-	})
+    chrome.storage.sync.remove(['userText'], function() {
+                               document.getElementById("convertedUrl").textContent = ""
+                               document.getElementById("link").value = ""
+                               })
 }
 
 //open links
 document.addEventListener('DOMContentLoaded', function() {
-   let openTab = document.getElementById("linkTab");
-   //simpler version instrad of using callback/async method
-   openTab.onclick = function() {
-   		chrome.tabs.create({active: true, url: openTab.textContent});
-   }
-
-   //y.addEventListener("click", function() {
-   	//	chrome.tabs.create({active: true, url: y.textContent});
-   //});
-})
+                          let openTab = document.getElementById("linkTab");
+                          //simpler version instrad of using callback/async method
+                          openTab.onclick = function() {
+                          chrome.tabs.create({active: true, url: openTab.textContent});
+                          }
+                          
+                          //y.addEventListener("click", function() {
+                          //    chrome.tabs.create({active: true, url: y.textContent});
+                          //});
+                          })
 
 function openIndex() {
- chrome.tabs.create({active: true, url: control.textContent});
+    chrome.tabs.create({active: true, url: control.textContent});
 }
