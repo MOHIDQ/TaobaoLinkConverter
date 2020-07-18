b834c35f3e8d49ee201dcf44af0e95deff19a363
mohidqureshi <mohidq99@gmail.com>
2020-07-18 13:17:40 -0400

    Fixed bug where link div wouldn't clear

--- popup.js
+++ popup.js
@@ -4,32 +4,54 @@ let clear = document.getElementById('clearButton')
 chrome.storage.sync.get(['userText'], function(result) {
 	console.log(result)
 	let mainDiv = document.getElementById("newLinks")
-	for(let i = 0; i < result.userText.length; i++) {
-		let link = document.createElement("a")
-		link.href = result.userText[i]
-		link.textContent = result.userText[i]
-		link.className = "btn btn-link"
-		link.id = "link_" + i
-		link.addEventListener("click", function() {
-			chrome.tabs.create({active: true, url: link.textContent});
-		})
+	if(result.userText !== undefined) {
+		for(let i = 0; i < result.userText.length; i++) {
+			let line_break = document.createElement("BR")
+			let link = document.createElement("a")
+			link.href = result.userText[i]
+			link.textContent = result.userText[i]
+			link.className = "btn btn-link"
+			link.id = "link_" + i
+			link.addEventListener("click", function() {
+				chrome.tabs.create({active: true, url: link.textContent});
+			})
 
-		mainDiv.appendChild(link)
-		mainDiv.appendChild(line_break)
+			mainDiv.appendChild(link)
+			mainDiv.appendChild(line_break)
+		}
 	}
 	//document.getElementById('convertedUrl').textContent = result.userText
 
 });
 
+function clearLinkDiv() {
+	let linkDivChild = document.getElementById("newLinks").children
+	let childArr = []
+	let len = linkDivChild.length
+	for(let i = 0; i < len; i++) {
+		childArr.push(linkDivChild[i])
+	}
+	for(let i = 0; i < len; i++) {
+		childArr[i].remove()
+	}
+}
+
 //function to convert mobile link to web link
 convertLink.onclick = function() {
 	let defaultLink = "https://item.taobao.com/item.htm?id="
-	let arrayLink = document.getElementById('link').value.split(",")
-	let preURL = document.getElementById('link').value
+	let arrayLink = document.getElementById('linkArea').value.split(",")
 	let id = "";
 	let validLinks = []
 
 	let mainDiv = document.getElementById("newLinks")
+	clearLinkDiv()
+	
+
+	chrome.storage.sync.remove(['userText'], function() {
+		document.getElementById("linkArea").value = ""
+	})
+
+	//checking each of  the links to see if valid or not
 	for(let i = 0; i < arrayLink.length; i++) {
 		if(!arrayLink[i].includes("id=")) {
 			let line_break = document.createElement("BR")
@@ -59,10 +81,10 @@ convertLink.onclick = function() {
 
 			validLinks.push(defaultLink + id)
 		}
-		
+
 	}
 
-	console.log(arrayLink)
+	console.log(validLinks)
 
 	chrome.storage.sync.set({userText: validLinks}, function() {
           
@@ -72,14 +94,10 @@ convertLink.onclick = function() {
 //when clear button id clicked
 clear.onclick = function() {
 	chrome.storage.sync.remove(['userText'], function() {
-		document.getElementById("link").value = ""
-		document.getElementById("errorMessage").textContent = ""
+		document.getElementById("linkArea").value = ""
 
-		let linkDivChild = document.getElementById("newLinks").children
-		for(let i = 0; i < linkDivChild.length; i++) {
-			linkDivChild[i].remove()
-		}
 	})
+	clearLinkDiv()
 }
-
+//MAKE FUNCTIONAILITY WHERE IT SAVES EVERY SINGLE LINK CONVERTED AND A BUTTON TO LIST THEM ALL OR DELETE THEM ALL
 //https://world.taobao.com/item/547456053621.htm?fromSite=main
