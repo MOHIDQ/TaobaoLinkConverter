66126a514f3e52755db48506bd4a63afa02775c4
mohidqureshi <mohidq99@gmail.com>
2020-07-19 02:52:27 -0400

    Saves text area when extension closes

--- popup.js
+++ popup.js
@@ -24,6 +24,16 @@ chrome.storage.sync.get(['userText'], function(result) {
 
 });
 
+chrome.storage.sync.get(['textArea'], function(result) {
+	if(result.textArea !== undefined) {
+		console.log(result)
+		document.getElementById("linkArea").value = result.textArea
+	}
+	//document.getElementById('convertedUrl').textContent = result.userText
+
+});
+
+//function that clears the div that contain the converted links
 function clearLinkDiv() {
 	let linkDivChild = document.getElementById("newLinks").children
 	let childArr = []
@@ -48,12 +58,13 @@ convertLink.onclick = function() {
 	
 
 	chrome.storage.sync.remove(['userText'], function() {
-		document.getElementById("linkArea").value = ""
+	})
+	chrome.storage.sync.remove(['textArea'], function() {
 	})
 
 	//checking each of  the links to see if valid or not
 	for(let i = 0; i < arrayLink.length; i++) {
-		if(!arrayLink[i].includes("id=")) {
+		if(!arrayLink[i].includes("id=") && !arrayLink[i].includes("item/") && arrayLink[i] !== "") {
 			let line_break = document.createElement("BR")
 			let newLink = document.createElement("a")
 			newLink.className = "btn btn-link text-danger"
@@ -61,9 +72,28 @@ convertLink.onclick = function() {
 			mainDiv.appendChild(newLink)
 			mainDiv.appendChild(line_break)
 		}
-		else {														
-			
+		else if (arrayLink[i].includes("id=")) {														
 			let index = arrayLink[i].indexOf("id=") + 3
+			id = arrayLink[i].substring(index, index + 12)
+			let line_break = document.createElement("BR")
+
+			let link = document.createElement("a")
+			link.href = defaultLink + id
+			link.textContent = defaultLink + id
+			link.className = "btn btn-link"
+			link.id = "link_" + i
+			link.addEventListener("click", function() {
+				chrome.tabs.create({active: true, url: link.textContent});
+			})
+ 
+			mainDiv.appendChild(link)
+			mainDiv.appendChild(line_break)
+
+			validLinks.push(defaultLink + id)
+		}
+		//example of this condition would be https://world.taobao.com/item/547456053621.htm?fromSite=main
+		else if (arrayLink[i].includes("item/")) {
+			let index = arrayLink[i].indexOf("item/") + 5
 			id = arrayLink[i].substring(index, index + 12)
 			let line_break = document.createElement("BR")
 
@@ -84,10 +114,9 @@ convertLink.onclick = function() {
 
 	}
 
-	console.log(validLinks)
-
+	//saves the stored links 
 	chrome.storage.sync.set({userText: validLinks}, function() {
-          
+        document.getElementById("linkArea").value = ""
     });
 }
 
@@ -99,5 +128,15 @@ clear.onclick = function() {
 	})
 	clearLinkDiv()
 }
+
+//saves the text area value if extension pop up is still closed
+window.onblur = function(){
+	chrome.storage.sync.set({textArea: document.getElementById("linkArea").value}, function() {
+    
+    });
+}
+
+
+
+
 //MAKE FUNCTIONAILITY WHERE IT SAVES EVERY SINGLE LINK CONVERTED AND A BUTTON TO LIST THEM ALL OR DELETE THEM ALL
-//https://world.taobao.com/item/547456053621.htm?fromSite=main
