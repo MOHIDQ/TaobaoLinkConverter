34ff9af48a87943f6464c03f86d700f6bcb8740a
mohidqureshi <mohidq99@gmail.com>
2020-08-01 16:16:07 -0400

    Added all link conversions for ContextMenus

--- eventPage.js
+++ eventPage.js
@@ -11,12 +11,27 @@ function convertLink(preUrl) {
 }
 
 function goToLink(preUrl) {
-	console.log(preUrl)
 	let defaultLink = "https://item.taobao.com/item.htm?id="
+	let id = ""
+	//regular taobao link
+	if (preUrl.includes("id=")) {
+		let index = preUrl.indexOf("id=") + 3
+		id = preUrl.substring(index, index + 12)
+		chrome.tabs.create({ url: convertLink(defaultLink + id) })
+	}
+	//world taobao links
+	else if (preUrl.includes("item/")) {
+		let index = preUrl.indexOf("item/") + 5
+		id = preUrl.substring(index, index + 12)
+		chrome.tabs.create({ url: convertLink(defaultLink + id) })
+	}
+	//superbuy links
+	else if(preUrl.includes("id%")) {
+		let index = preUrl.indexOf("id%") + 5 //+ 3 + 2 since we dont need the first two digits
+		id = preUrl.substring(index, index + 12)
+		chrome.tabs.create({ url: convertLink(defaultLink + id) })
+	}
 
-	//let newURL = convertLink(preUrl)
-  	chrome.tabs.create({ url: convertLink(preUrl) })
-  	//return newURL
 }
 
 chrome.contextMenus.create(contextMenuItem);
